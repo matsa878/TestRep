f23b6e68fd43dfd8af5614f025436f55cdd3a741
Mats Sandin <mazy.wicked@gmail.com>
2012-10-18 15:47:29 +0200

    more stuff

--- Hello World/src/HELLOWORLD/hello_world.java
+++ Hello World/src/HELLOWORLD/hello_world.java
@@ -8,6 +8,7 @@ public class hello_world {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello world!");
+			System.out.println("Hello the other world!");
 	}
 
 }
